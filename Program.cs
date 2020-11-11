2442a1ed5eb5cd736d80785646afab9ddcc1217e
Your Name <renato_inovacao@hotmail.com>
2020-11-11 12:26:55 -0300

    Projeto Lógica de Programação

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Logica_de_programacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aqui vai o cosigo
+           // Console.WriteLine("Digite o seu nome:");
+           // string nomeCompleto = Console.ReadLine();
+
+
+            //Console.WriteLine("Bem vindo "+nomeCompleto);
+            
+            int valorUm;
+            int valorDois;
+
+            Console.WriteLine("Soma de dois números");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Digite o primeiro valor");
+            valorUm = int.Parse(Console.ReadLine());
+            Console.Write ("Digite o segundo valor: ");
+            valorDois = int.Parse(Console.ReadLine());
+            
+            //Processamento
+            int soma = valorUm + valorDois;
+            
+            //Exbir
+
+            Console.WriteLine("A Soma de "+valorUm+" + "+valorDois+" = "+soma);
+            
+        }
+    }
+}
+ 
